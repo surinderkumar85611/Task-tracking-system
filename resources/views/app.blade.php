f0cc1a6887d23fadbb953360cd09abc3465b9803
surinderkumar85611 <surinderkumar85611@gmail.com>
2026-05-27 08:31:10 +0530

    Initial comit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+
+    <div id="app">
+        <h1>@{{ message }}</h1>
+    </div>
+
+</body>
+</html>
