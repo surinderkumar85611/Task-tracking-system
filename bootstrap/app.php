e53df2ea15b6ead405a8da0840b385874a5f5087
surajm26667 <surajm26667@gmail.com>
2026-05-28 09:28:18 +0530

    fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,12 +10,11 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-   ->withMiddleware(function (Middleware $middleware): void {
-    $middleware->web(append: [
-        \App\Http\Middleware\HandleInertiaRequests::class,
-    ]);
-
-    $middleware->alias([
-        'admin' => \App\Http\Middleware\AdminMiddleware::class,
-    ]);
+    ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->web(append: [
+            \App\Http\Middleware\HandleInertiaRequests::class,
+        ]);
     })
+    ->withExceptions(function (Exceptions $exceptions): void {
+        //
+    })->create();
